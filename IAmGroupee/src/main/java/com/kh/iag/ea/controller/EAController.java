88397c53f0eb935e66921a8ad1a10a33fb88a603
이종훈 <aljongjong@gmail.com>
2022-02-22 16:03:56 +0900

    <전자결재> [Style] 기안문서 조회페이지 UI 구현

--- IAmGroupee/src/main/java/com/kh/iag/ea/controller/EAController.java
+++ IAmGroupee/src/main/java/com/kh/iag/ea/controller/EAController.java
@@ -8,12 +8,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/ea")
 public class EAController {
 	
+	// 기안신청 (양식선택)
 	@GetMapping(value = "/signup")
 	public String signUp() {
-		return "ea/signup";
+		return "ea/user/ea_signup_form";
 	}
+	// 기안신청 (기안작성)
 	@GetMapping(value = "/write")
 	public String write() {
-		return "ea/write";
+		return "ea/user/ea_signup_write";
 	}
+	
+	// 기안문서조회 (리스트)
+	@GetMapping(value = "/signuplist")
+	public String signuplist() {
+		return "ea/user/ea_signuplist_list";
+	}
+	// 기안문서조회 (상세조회)
+//	@GetMapping(value = "/")
+//	public String signuplistDetail() {
+//		return "ea/user/ea_signuplist_detail";
+//	}
 }
